4d2d4f51d01830001fd78c1a3e1344a72ce1ba96
PG066149 <Pavan.GM@cerner.com>
2020-11-25 20:54:48 +0530

    initial commit

---
+++ src/components/EmpApplication.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class EmpApplication extends React.Component {
+
+    render() {
+        return (
+            <EmpContainer />
+        );
+    }
+
+}
+
+export default EmpApplication
